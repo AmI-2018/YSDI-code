330133b89dd52509cb017091c2f528446b49e150
Oliviero Vouch <s226777@studenti.polito.it>
2018-06-11 19:47:42 +0200

    updated codes
    Modification of the Arduino codes:
    - Variable resistor  sensor for detection of student sitting on the chair has been interfaced with WiFi network;
    - Load cell code modified to adapt to the web-server

--- Arduino_codes/PRESSURE_SENSOR/pressure_sensor.ino
+++ Arduino_codes/PRESSURE_SENSOR/pressure_sensor.ino
@@ -1,34 +1,33 @@
 /*AMI 2018 YSDI
- * 2018 May 25
- * Load Cell HX711 Module Interface with Arduino to measure weight in Kgs
- Arduino 
- pin 
- 2 -> HX711 CLK
- 3 -> DATA_OUT
- 5V -> VCC (POWER SUPPLY)
- GND -> GND (GROUND)
  
- Most any pin on the Arduino Uno will be compatible with DOUT/CLK.
- The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
+ -This function has to decide if the Arduino has to send a zero or a one to the raspberry
 */
  
 #include "HX711.h"  //You must have this library in your arduino library folder
  
-#define DOUT  3
-#define CLK  2
- 
+#define DOUT  A0
+#define CLK  A2
+#define N 4
+// 336355
 HX711 scale(DOUT, CLK);
+
+// Functions' prototypes
+void communicate_data(void);
  
 //Change this calibration factor as per your load cell once it is found you many need to vary it in thousands
 float calibration_factor = 336355 ;  
- 
+float reading[N];
+int count=0;
+int flag;//this variable is used as  a flag: =1 means that a variation wrt the threshold has been detected; =0 means that nothing has benn detected 
 //=============================================================================================
 //                         SETUP
 //=============================================================================================
+
+   
 void setup() {
+  
   Serial.begin(9600);
   Serial.println("HX711 Calibration");
-  Serial.println("Remove all weight from scale");
   Serial.println("After readings begin, place known weight on scale");
   Serial.println("Press a,s,d,f to increase calibration factor by 10,100,1000,10000 respectively");
   Serial.println("Press z,x,c,v to decrease calibration factor by 10,100,1000,10000 respectively");
@@ -37,8 +36,15 @@ void setup() {
   scale.tare(); //Reset the scale to 0
  
   long zero_factor = scale.read_average(); //Get a baseline reading
+  Serial.println("Remove all weight from scale");
   Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
   Serial.println(zero_factor);
+  int i;
+  for(i=0;i<N;i++)
+  {
+   reading[i] = 0;
+  }
+
 }
  
 //=============================================================================================
@@ -54,6 +60,20 @@ void loop() {
   Serial.print(" calibration_factor: ");
   Serial.print(calibration_factor);
   Serial.println();
+ 
+  reading[count]= scale.get_units();    
+  if (count == (N-1))
+   { 
+      //we want to sample N values
+      communicate_data();
+      if (flag == 1)
+      {
+       Serial.println("Variation");
+       Serial.println(flag); //SEND A 1 TO THE RASPBERRY
+      }
+      count=-1;//resetting the counter variable
+   }
+  count++;
  
   if(Serial.available())
   {
@@ -75,10 +95,29 @@ void loop() {
     else if(temp == 'v')
       calibration_factor -= 10000;  
     else if(temp == 't')
+    {
+      Serial.println();
       scale.tare();  //Reset the scale to zero
+    }
   }
-  delay(3500);
+  delay(250);
 }
 //=============================================================================================
-
-
+//Defining communicate_data function
+void communicate_data()
+{
+  int i;
+  float threshold =0.025;//THIS HAS TO BE CHECKED IN LBA TO GET SOME REASONABLE VALUES
+  for(i=0;i<N-1;i++){
+  if((abs(reading[i]-reading[i+1]))>threshold)
+  {
+    flag=1;
+    return;
+  }
+  else
+  {
+    flag=0; 
+  }
+  }
+  return; 
+}
--- Arduino_codes/VARIABLE_RESISTANCE_SENSOR/IEFSR_SENSOR/IEFSR_SENSOR.ino
+++ Arduino_codes/VARIABLE_RESISTANCE_SENSOR/IEFSR_SENSOR/IEFSR_SENSOR.ino
@@ -1,25 +1,48 @@
-int tmp=0;
-float threshold = 0.10;
+#include <Bridge.h>
+#include <HttpClient.h>
+#include <YunClient.h>
+#include <SPI.h>
+
+/// GLOBAL VARIABLES ///
+int tmp=0; // auxiliary temporary variable
+float threshold = 0.10; // refernce threshold needed to evaluate our data
+
+/// IPAddress of the Raspberry server(192,168,0,21); ///
+IPAddress rasp_server(192,168,0,21); // specify destination server IP of Raspberry
+
+YunClient client;
+
+///// FUNCTIONS' PROTOTYPES /////////////////////////////////////////////////////////////////////////
+
+void send_to_rasp(int); // function to generate HTTP request to raspberry server with POST method
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// the setup routine runs once when you press reset:
+/// SET-UP FUNCTION
+
 void setup() {
-// initialize serial communication at 9600 bits per second:
-Serial.begin(9600);
+  // put your setup code here, to run once:
+  Bridge.begin();
+  Serial.begin(9660); // Baud-rate --> D = 9,66 Ksymbols/s
+
 }
 
+// LOOP FUNCTION
 
-// the loop routine runs over and over again forever:
 
 void loop() {
 
-// read the input on analog pin 0:
+// read the input on analog pins 0 and 1:
 int sensorValue = analogRead(A0);
 int sensorValue_1 = analogRead(A1);
-int difference = sensorValue - sensorValue_1 ;
+int difference = sensorValue - sensorValue_1 ; // evaluate the voltage difference between the sensor and the resistor
 // Convert the analog reading (which goes from 0 – 1023) to a voltage (0 – 5V):
 float voltage = difference * (5.0 / 1023.0);
+int value=0;
+
 //Serial.println(voltage);
+
 if (voltage > threshold)
 {
  tmp = tmp + 1;
@@ -27,7 +50,8 @@ if (voltage > threshold)
 
 if(tmp == 1)
  {
-  Serial.println(1); // print out the value you read when first reading of some weigth;
+  value = 1;
+  send_to_rasp(value); // send a message to the server informing it when first reading of some weigth greater than threshold;
   tmp = tmp + 1;
  }
 else
@@ -35,9 +59,47 @@ else
   if (tmp > 1 && voltage <= threshold)
   {
     tmp = 0;
-    Serial.println(0); // print out the value you read when first reading of some weigth;
+    value=0;
+    Serial.println(threshold);
+    send_to_rasp(value); // send to raspberry infromation that a null weight is read --> nobody sits on the chair
   }
 }
-delay(1500);
+delay(1500); // delay the next iteration of the loop by 1.5 milliseconds
 }
+
+//////////////////// SEND TO RASP FUNCTION /////////////////////////////////
+
+void send_to_rasp(int value)
+{
+  String JsonData = "{\"value\": ";
+  JsonData = JsonData + value;
+  JsonData = JsonData + "}";
+  Serial.println(threshold);
+  client.connect("192.168.0.21", 8080);
+  if(client.connected()) // open socket on port 8080
+  { 
+        Serial.println("Connected");  // serial print --> debugging purpose      
+        client.println("POST /arduino HTTP/1.1"); // maybe specify resource
+        client.println("Host:192.168.0.21"); // SPECIFY HOST!!!!
+        client.print("Content-Length: ");
+        client.println(JsonData.length());
+        client.println("User-Agent: Arduino/1.0");
+        client.println("Accept: application/json");
+        Serial.println(JsonData.length());
+        client.println("Content-Type: application/json");
+        client.println("Connection: close");
+        client.println();
+        client.println(JsonData);
+         
+   } 
+    
+  else // if connection fails
+  {
+      Serial.println("connection failed");
+      Serial.println();
+      Serial.println("disconnecting.");
+      client.stop();
+   }
+}
+
 
